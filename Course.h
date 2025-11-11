31f951ffac5e4565bb77b0b8d7436d9b5885b993
Ahmed-Saadallah1 <ahmedsaadallah164@gmail.com>
2025-11-11 09:14:06 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,30 @@
 #ifndef COURSE_H
 #define COURSE_H
-
+#include <iostrem>
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+
+class Course {
 
 
+private :
+string courseCode ;
+string courseName;
+int maxStudents;
+Student* students;
+int currentStudents;
 
-
-
-
-
-
-
-
-
-
-
-
+public :
+Course(string courseCode ,string courseName , int maxStudents ,Student* students, int currentStudents);
+Course();
+~Course();
+void addStudent(const Student& s);
+displayCourseInfo();
+};
 #endif
+
+
+
+
+
