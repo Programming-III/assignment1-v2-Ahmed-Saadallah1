1a8dcd696ab0abba324a37ed613df283a3af07f5
Ahmed-Saadallah1 <ahmedsaadallah164@gmail.com>
2025-11-11 09:04:27 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -3,8 +3,23 @@
 #include <string>
 #include <iostream>
 #include "Person.h"
-#include <string>
+
 #using namespace std;
+ class Student {
+
+private :
+int yearLevel;
+string major;
+
+public :
+Student (int yearLevel , string major );
+Student ();
+~Student();
+display();
+
+};
+#endif
+
 //#write student class here
 
 
